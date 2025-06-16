e1158478d477941a44ba09c57fb3f0da8f183efb
JulienDrx <julien_drieux@hotmail.com>
2025-06-16 17:55:24 +0200

    Ajout d'une fonction executable pour utilisation du projet

--- app.py
+++ app.py
@@ -3,15 +3,29 @@ import sqlite3
 from pathlib import Path
 from Tableau_alternance import creer_table_si_absente, importer_csv_si_table_vide
 from curl_url import sauvegarder_page_web_curl
+import webbrowser
+import threading
+import sys
+import time
+import os
+import tempfile
 
-db_path = Path(__file__).resolve().parent / "BDD_alternance.db"
 
-app = Flask(__name__)
+if getattr(sys, 'frozen', False):
+    base_path = Path(sys._MEIPASS)
+else:
+    base_path = Path(__file__).parent.resolve()
+
+db_path = base_path / "BDD_alternance.db"
+template_folder = base_path / "templates"
+
+app = Flask(__name__, template_folder=str(template_folder))
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
         # Récupération des données du formulaire
         entreprise = request.form['entreprise']
@@ -72,7 +86,39 @@ def modifier(id):
         con.close()
         return render_template('modifier.html', ligne=ligne)
 
+
+
+_browser_opened = False
+_browser_lock = threading.Lock()
+
+def open_browser():
+    global _browser_opened
+    print("attente ouverture navigateur")
+    time.sleep(3)
+    with _browser_lock:
+            if not _browser_opened:
+                print("Ouverture du navigateur")
+                webbrowser.open_new("http://127.0.0.1:5000")
+                _browser_opened = True
+            else:
+                print("Navigateur déjà ouvert, pas besoin de le rouvrir.")
+
+
+
+
+
+def run_app():
+    print("Lancement de l'application Flask")
+    app.run(debug=False,use_reloader = False)
+    
+
+
 if __name__ == '__main__':
     creer_table_si_absente(db_path)
     importer_csv_si_table_vide("données_de_base.csv", db_path)
-    app.run(debug=True)
+
+    threading.Thread(target=run_app).start()
+    open_browser()
+
+    while True:
+        time.sleep(1)
